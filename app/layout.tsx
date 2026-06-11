7ce26a59493b9274b9b70f501170d7d1a0dccd09
datapulta <datapulta@gmail.com>
2026-06-11 11:54:55 -0600

    feat: add facebook pixel for meta ads tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { FloatingWhatsApp } from '@/components/floating-whatsapp'
 import { OrganizationJsonLd } from '@/components/structured-data'
 import { siteConfig } from '@/lib/seo-config'
 import './globals.css'
+import Script from 'next/script'
 
 const geistSans = Geist({
   subsets: ["latin"],
@@ -138,6 +139,30 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
       <body className="font-sans antialiased min-h-screen">
+        {/* Facebook Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1744889803343478');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1744889803343478&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <OrganizationJsonLd />
         {children}
         <FloatingWhatsApp />
